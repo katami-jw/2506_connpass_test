8bdcc69db551c4a5e7ace4bae873390fed545097
miyuki katayama <165630128+katami-jw@users.noreply.github.com>
2025-05-29 23:17:52 +0900

    Update flask_app.py

--- flask_app.py
+++ flask_app.py
@@ -1,10 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def home():
-    return render_template("index.html")
-
-if __name__ == "__main__":
-    app.run()
+@app.route('/')
+def hello_world():
+    return 'Hello from Flask!'
